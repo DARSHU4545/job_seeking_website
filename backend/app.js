1f80510ee4af3c1b92cac101d0797bd2f78a944a
Darshan S <hitmandarshan34@gmail.com>
2024-05-21 21:18:47 +0530

    cammit10

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,10 @@ config({ path: ".env" });
 
 app.use(
   cors({
-    origin: ["https://job-seeking-website-darshan.vercel.app"],
+    origin: [
+      "https://job-seeking-website-darshan.vercel.app",
+      "http://localhost:5173",
+    ],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
@@ -24,10 +27,12 @@ app.use(function (req, res, next) {
   //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-  });
-
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
